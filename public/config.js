c7aa79e2aa23572095c26426f9c0f2a6be3db0bc
Mario Reder <mario.reder@pm.me>
2025-11-05 05:21:15 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Swap,Markets,Leaderboard,Rewards",
-  "VITE_CUSTOM_MENUS": "",
+  "VITE_CUSTOM_MENUS": "SWAP, https://swap.novax.meme",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://dex.orderly.network/",
   "VITE_DISCORD_URL": "https://dex.orderly.network/",
